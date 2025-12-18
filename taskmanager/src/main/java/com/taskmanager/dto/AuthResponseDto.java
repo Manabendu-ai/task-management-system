618a84063e6c92b7dcd46f3de4106e500894336f
yash <yashkhakhkhar455@gmail.com>
2025-12-18 17:41:02 +0530

    intialized repo and created upto security config.

---
+++ taskmanager/src/main/java/com/taskmanager/dto/AuthResponseDto.java
@@ -0,0 +1,19 @@
+package com.taskmanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AuthResponseDto {
+
+    private String token;
+    private String message;
+
+}
